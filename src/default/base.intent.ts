a2ed90bd40e23fec6fbd2ab8133f5b3ad1613b69
NalenyNadessane <n.naleny@gmail.com>
2020-02-12 14:22:44 +0100

    modification de l'intent song retour de base

--- src/default/base.intent.ts
+++ src/default/base.intent.ts
@@ -29,12 +29,12 @@ export const SongIntent  = (conv: DialogflowConversation) => {
     }
     conv.ask('Ok voici une chanson.');
     conv.ask(new MediaObject({
-      name: 'Demi portion',
-      url: 'https://cdns-preview-8.dzcdn.net/stream/c-804d4dcfd35a1ab0d093ffd4b8a1a169-3.mp3',
-      description: 'Mon dico vol.5',
+      name: 'Jazz in Paris',
+      url: 'https://storage.googleapis.com/automotive-media/Jazz_In_Paris.mp3',
+      description: 'A funky Jazz tune',
       icon: new Image({
-        url: 'https://cdns-images.dzcdn.net/images/cover/eb2d6a1268f8f6fef01e8b1cc01bc829/56x56-000000-80-0-0.jpg',
-        alt: 'Album la bonne école',
+        url: 'https://storage.googleapis.com/automotive-media/album_art.jpg',
+        alt: 'Album cover of an ocean view',
       }),
     }));
   };
