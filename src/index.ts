15a7bc4a893c96153e4eee782c9b10d4d3136054
NalenyNadessane <n.naleny@gmail.com>
2020-02-12 16:27:02 +0100

    creation intent Media Status test

--- src/index.ts
+++ src/index.ts
@@ -39,5 +39,14 @@ app.intent('Wesh', weshIntent)
 app.intent('Song', SongIntent)
 
 app.intent('Song2', Song2Intent)
+app.intent('Media Status', (conv) => {
+  const mediaStatus = conv.arguments.get('MEDIA_STATUS');
+  let response = 'Unknown media status received.';
+  if (mediaStatus && mediaStatus.status === 'FINISHED') {
+    response = 'Hope you enjoyed the tune!';
+  }
+  conv.ask(response);
+  conv.ask('Which response would you like to see next?');
+});
 
 express().use(bodyParser.json(), app).listen(8080);
